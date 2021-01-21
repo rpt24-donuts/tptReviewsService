cfd5a0f3de2319c93ca5bfd4fd589908cf17d6d9
Matteo Besio <matteo.besio@gmail.com>
2021-01-20 23:31:46 -0800

    removed console logs

--- server/models.js
+++ server/models.js
@@ -2,8 +2,6 @@ const { review } = require('./mongodb.js');
 
 const model = {
   put: (reviewId, updateFields, callback) => {
-    // validate grade and standard inputs
-    console.log('updateFields ', updateFields);
     review.findOneAndUpdate(reviewId, updateFields, { new: true, useFindAndModify: false })
       .then((results) => {
         callback(null, results);
