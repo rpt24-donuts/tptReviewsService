da1684e7e7a516ec3a7ea7f2f805f787711ec318
Matteo Besio <matteo.besio@gmail.com>
2021-01-22 22:14:03 -0800

    added some input handling in the put endpoint

--- server/models.js
+++ server/models.js
@@ -2,13 +2,31 @@ const { review } = require('./mongodb.js');
 
 const model = {
   put: (reviewId, updateFields, callback) => {
-    review.findOneAndUpdate(reviewId, updateFields, { new: true, useFindAndModify: false })
-      .then((results) => {
-        callback(null, results);
-      })
-      .catch((err) => {
-        callback(err);
-      });
+    // handle incorrect inputs
+    const wrongFormat = 'wrong format';
+    const typeOfTitle = typeof updateFields.title; // should be string
+    const typeOfDescription = typeof updateFields.description; // should be string
+    const standardsIsArray = Array.isArray(updateFields.standards); // should be array
+    const gradeIsArray = Array.isArray(updateFields.standards); // should be array
+
+    if (typeOfTitle !== 'string') {
+      callback(wrongFormat);
+    } else if (typeOfDescription !== 'string') {
+      callback(wrongFormat);
+    } else if (!standardsIsArray) {
+      callback(wrongFormat);
+    } else if (!gradeIsArray) {
+      callback(wrongFormat);
+    } else {
+      // if all inputs have the valid format, then update the entry in the database
+      review.findOneAndUpdate(reviewId, updateFields, { new: true, useFindAndModify: false })
+        .then((results) => {
+          callback(null, results);
+        })
+        .catch((err) => {
+          callback(err);
+        });
+    }
   },
 };
 
